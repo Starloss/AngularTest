96367a86a61f8bcafed877f33edbebca86943e51
Diego Rangel <starloss09@gmail.com>
2020-02-26 11:33:19 -0400

    Commit inicial, estructura con enrutamiento, header y footer creda

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { CharactersComponent } from './pages/characters/characters.component';
+import { FilmsComponent } from './pages/films/films.component';
+import { StarshipsComponent } from './pages/starships/starships.component';
+import { VehiclesComponent } from './pages/vehicles/vehicles.component';
+import { SpeciesComponent } from './pages/species/species.component';
+import { PlanetsComponent } from './pages/planets/planets.component';
+import { ConfigurationComponent } from './pages/configuration/configuration.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    CharactersComponent,
+    FilmsComponent,
+    StarshipsComponent,
+    VehiclesComponent,
+    SpeciesComponent,
+    PlanetsComponent,
+    ConfigurationComponent
   ],
   imports: [
     BrowserModule,

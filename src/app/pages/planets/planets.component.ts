04c00eac6823b842b2291155a7eb1376db703db6
Diego Rangel <starloss09@gmail.com>
2020-02-28 02:00:00 -0400

    Planets page Done.

--- src/app/pages/planets/planets.component.ts
+++ src/app/pages/planets/planets.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { SwapiService } from 'src/app/services/swapi.service';
-import { Planets } from 'src/app/interfaces/swapi.interfaces';
+import { Planets, Planet } from 'src/app/interfaces/swapi.interfaces';
 
 @Component({
   selector: 'app-planets',
@@ -9,15 +9,40 @@ import { Planets } from 'src/app/interfaces/swapi.interfaces';
 })
 export class PlanetsComponent implements OnInit {
 
-  planets: Planets;
-  next = '';
-  prev = '';
+  planets: Planet[];
+  next: string = null;
+  prev: string = null;
 
   constructor( public swapi: SwapiService ) { }
 
   ngOnInit() {
-    this.swapi.getPlanets().subscribe( (resp: any) => {
-      this.planets = resp;
+    this.getPlanets();
+  }
+
+  navigateForward() {
+    this.swapi.navigate(this.next).subscribe( (resp: Planets) => {
+
+      this.planets = resp.results;
+      this.next = resp.next;
+      this.prev = resp.previous;
+    });
+  }
+
+  navigateBack() {
+    this.swapi.navigate(this.prev).subscribe( (resp: Planets) => {
+
+      this.planets = resp.results;
+      this.next = resp.next;
+      this.prev = resp.previous;
+    });
+  }
+
+  getPlanets() {
+    this.swapi.getPlanets().subscribe( (resp: Planets) => {
+
+      this.planets = resp.results;
+      this.next = resp.next;
+      this.prev = resp.previous;
     });
   }
 
